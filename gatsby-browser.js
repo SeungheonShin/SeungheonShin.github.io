245f90928836c33b917b127569e291a720cfc8ee
SeungheonShin <gody8756@naver.com>
2022-10-22 14:12:01 +0900

    [fix] Link 이동 시 스크롤 위치 유지

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,15 +6,6 @@
 
 // You can delete this file if you're not using it
 import 'prismjs/themes/prism-okaidia.css';
+import './static/styles/global.css';
 
-
-export const shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition
-}) => {
-  const currentPosition = getSavedScrollPosition(location);
-
-  window.scrollTo(...(currentPosition || [0, 0]));
-
-  return false
-}
+export const shouldUpdateScroll = () => false
